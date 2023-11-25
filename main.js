80ad4c41a13282d51ac8d8a9fea86ccb594bec4f
ak3zaidan <74563626+ak3zaidan@users.noreply.github.com>
2023-11-25 14:20:57 -0800

    Add files via upload
    Final Pantry no bugs and complete grocery list

--- main.js
+++ main.js
@@ -74,11 +74,6 @@ let currentAccount = null;  //store current account
 var currentDay = null;  //store the current day
 var recipeArray = []; //store all reciepies
 
-//example to create a new ingredient will look similar to all constructors
-//const ingredient1 = new Ingredient();
-//ingredient1.name = 'Flour';
-//ingredientArray.push(ingredient1);
-
 
 // reads a file string and turns it into a recipe struct 
 function read_file(file){ 
@@ -125,26 +120,14 @@ function populateRecipies() {
     });
   });
 }
-function populateingredients() {
-  console.log('hi');
-}
+
 function setCurrentAccount() {
   const userAccount = new Account();
   userAccount.username = 'TestUser';
   userAccount.picture = 'testpfp.jpg';
   self.currentAccount = userAccount;
 }
-function deleteRecipe(index) {
-  if (index >= 0 && index < recipeArray.length) {
-    recipeArray.splice(index, 1); 
-  }
-}
-function createRecipe() {
-  console.log('hi');
-}
-function editRecipe() {
-  console.log('hi');
-}
+
 function createIngredient(data) {
   let list = data.split(",");
   let temp = [];
@@ -161,16 +144,7 @@ function createIngredient(data) {
   }
   return arr;
 }
-function deleteingredient(index) {
-  if (index >= 0 && index < ingredientArray.length) {
-    ingredientArray.splice(index, 1); 
-  }
-}
-function editingredient(index) {
-  if (index >= 0 && index < ingredientArray.length) {
-    //ingredientArray[index].
-  }
-}
+
 
 
 
@@ -192,10 +166,10 @@ class PantryItem {
 function edit_pantry_item(newItem) {
   const index = pantryList.findIndex(item => item.title === newItem.oldTitle); //find the index
   if (index !== -1) {
-    finalItem = new PantryItem(newItem.newTitle, newItem.nutritionFacts, newItem.expirationDate, newItem.quantity);
+    finalItem = new PantryItem(newItem.newTitle, newItem.nutritionFacts, new Date(newItem.expirationDate), newItem.quantity);
     pantryList[index] = finalItem;  //edit it in the array
     updatePantryFile('pantryItems.txt', pantryList);  //update the textfile
-    read_Pantry_List("pantryItems.txt"); //send new data to html
+    mainWindow.webContents.send('newPantryData', pantryList); //send new data to html
   } else {
     console.log(`Pantry item '${newItem.title}' not found.`);
   }
@@ -214,6 +188,7 @@ function delete_pantry_item(name) {
 }
 
 function add_pantry_item(newItem) {
+  newItem.expirationDate = new Date(newItem.expirationDate);
   pantryList.push(newItem);
   updatePantryFile('pantryItems.txt', pantryList);    //update the text file
   mainWindow.webContents.send('newPantryData', pantryList); //send new data to html
@@ -268,7 +243,120 @@ function updatePantryFile(filePath, pantryList) {
 
 
 
+var groceryList = []; // store all grocery items
 
+class GroceryItem {
+  constructor(title, quantity) {
+    this.title = title;
+    this.quantity = quantity;
+  }
+}
+
+function edit_Grocery_item(newItem) {
+  const index = groceryList.findIndex(item => item.title === newItem.oldTitle); //find the index
+  if (index !== -1) {
+    finalItem = new GroceryItem(newItem.newTitle, newItem.quantity);
+    groceryList[index] = finalItem;  //edit it in the array
+    updateGroceryFile('groceryListData.txt', groceryList);  //update the textfile
+    mainWindow.webContents.send('newGroceryData', groceryList); //send new data to html
+  } else {
+    console.log(`Grocery item '${newItem.title}' not found.`);
+  }
+}
+
+function delete_Grocery_item(name) {
+  const index = groceryList.findIndex(item => item.title === name);  //find the index
+  if (index !== -1) {
+    groceryList.splice(index, 1);  //remove it from array
+    updateGroceryFile('groceryListData.txt', groceryList);  //update the textfile
+    mainWindow.webContents.send('newGroceryData', groceryList); //send new data to html
+  } else {
+    console.log(`Grocery item '${name}' not found.`);
+  }
+}
+
+function add_Grocery_item(newItem) {
+  groceryList.push(newItem);
+  updateGroceryFile('groceryListData.txt', groceryList);  //update the textfile
+  mainWindow.webContents.send('newGroceryData', groceryList); //send new data to html
+  console.log(`Grocery item '${newItem.title}' added successfully.`);
+}
+
+function read_weekly_meals(){
+ // this function will read the weekly meal list and add ingrediants to the grocery list that are:
+ //     1. In the meal list and not in the grocery list or the pantry
+ //     2. low quantity items in pantry will also be added.
+  change = 0;
+  for (let i = 0; i < recipeArray.length; i += 1) {
+    for (let j = 0; j < recipeArray[i].ingredients.length; j += 1) {
+      const index = groceryList.findIndex(item => item.title === recipeArray[i].ingredients[j]);  //find the index
+      if (index === -1) {
+        const indexSec = pantryList.findIndex(item => item.title === recipeArray[i].ingredients[j]);  //find the index
+        if (indexSec === -1 || pantryList[indexSec].quantity === 0) {
+          newItem = new GroceryItem(recipeArray[i].ingredients[j], 1);
+          groceryList.push(newItem);
+          change = 1;
+        }
+      }
+    }
+  }
+  for (let i = 0; i < groceryList.length; i += 1) {
+    if (groceryList[i].quantity <= 1) {
+      groceryList[i].quantity = 2;
+      change = 1;
+    }
+  }
+  if (change == 1){
+    updateGroceryFile('groceryListData.txt', groceryList);  //update the textfile
+  }
+}
+
+function read_Grocery_List(filePath) {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    try {
+      if (err) {
+        throw err; // Throw the error to the catch block
+      }
+
+      const lines = data.split('\n');
+      const groceryItems = [];
+
+      for (let i = 0; i < lines.length; i += 2) {
+        const title = lines[i].trim();
+        const quantity = parseInt(lines[i + 1].trim());
+
+        const groceryItem = new GroceryItem(title, quantity);
+        groceryItems.push(groceryItem);
+      }
+
+      groceryList = groceryItems;
+
+      read_weekly_meals();
+
+      mainWindow.webContents.send('newGroceryData', groceryList);
+    } catch (error) {
+      console.error('Error reading grocery list file:', error);
+    }
+  });
+}
+
+function updateGroceryFile(filePath, groceryList) {
+  const lines = [];
+  groceryList.forEach(item => {  //put everything into an array
+    lines.push(item.title);
+    lines.push(item.quantity.toString());
+  });
+
+  const data = lines.join('\n');
+
+  fs.writeFile(filePath, data, 'utf8', (err) => {   //write the data to the file
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('Pantry file updated successfully.');
+    }
+  });
+}
 
 
 
@@ -290,24 +378,37 @@ const createWindow = () => {
 
   });
 
+
+  //init html files
   ipcMain.on('recipePageReady', () => {
     populateRecipies('testRecipe.txt');
   });
-
   ipcMain.on('pantryPageReady', () => {
     read_Pantry_List("pantryItems.txt");
   });
+  ipcMain.on('groceryPageReady', () => {
+    read_Grocery_List("groceryListData.txt");
+  });
 
+  //for pantry methods
   ipcMain.on('deletion', (event, name) => {
     delete_pantry_item(name);
   });
-
   ipcMain.on('updateItem', (event, newItem) => {
     edit_pantry_item(newItem);
   });
-
   ipcMain.on('createNewItem', (event, data) => {
       add_pantry_item(data);
+  });
+  //for Grocery methods
+  ipcMain.on('deletionGrocery', (event, name) => {
+    delete_Grocery_item(name);
+  });
+  ipcMain.on('updateGroceryItem', (event, newItem) => {
+    edit_Grocery_item(newItem);
+  });
+  ipcMain.on('createNewGroceryItem', (event, data) => {
+      add_Grocery_item(data);
   });
 };
 
